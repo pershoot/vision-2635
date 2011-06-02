342df3890b6fbaf2c3df4104f1f0ae7ca3612035
Prashant Somashekar <prashsomash@gmail.com>
2011-06-02 01:48:52 -0400

    msm: acpuclock-7x30: adjust AXI values for non ACPUCLOCK_OVERCLOCKING

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -201,19 +201,19 @@ static struct clkctl_acpu_speed acpu_freq_tbl[] = {
         { 1497600, PLL_2,   3, 0,  192000, 1300, VDD_RAW(1300) },
         { 1516800, PLL_2,   3, 0,  192000, 1300, VDD_RAW(1300) },
 #else
-	{ 24576,  SRC_LPXO, 0, 0,  30720000,  1000, VDD_RAW(1000) },
-	{ 61440,  PLL_3,    5, 11, 61440000,  1000, VDD_RAW(1000) },
-	{ 122880, PLL_3,    5, 5,  61440000,  1000, VDD_RAW(1000) },
-	{ 184320, PLL_3,    5, 4,  61440000,  1000, VDD_RAW(1000) },
-	{ MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440000, 1000, VDD_RAW(1000) },
-	{ 245760, PLL_3,    5, 2,  61440000,  1000, VDD_RAW(1000) },
-	{ 368640, PLL_3,    5, 1,  122800000, 1050, VDD_RAW(1050) },
-	{ 768000, PLL_1,    2, 0,  153600000, 1100, VDD_RAW(1100) },
+	{ 24576,  SRC_LPXO, 0, 0,  30720,  1000, VDD_RAW(1000) },
+	{ 61440,  PLL_3,    5, 11, 61440,  1000, VDD_RAW(1000) },
+	{ 122880, PLL_3,    5, 5,  61440,  1000, VDD_RAW(1000) },
+	{ 184320, PLL_3,    5, 4,  61440,  1000, VDD_RAW(1000) },
+	{ MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440, 1000, VDD_RAW(1000) },
+	{ 245760, PLL_3,    5, 2,  61440,  1000, VDD_RAW(1000) },
+	{ 368640, PLL_3,    5, 1,  122800, 1050, VDD_RAW(1050) },
+	{ 768000, PLL_1,    2, 0,  153600, 1100, VDD_RAW(1100) },
 #ifndef CONFIG_ACPUCLOCK_LIMIT_768MHZ
 	/* ACPU >= 806.4MHz requires MSMC1 @ 1.2V. Voting for
 	 * AXI @ 192MHz accomplishes this implicitly. 806.4MHz
 	 * is updated to 1024MHz at runtime for QSD8x55. */
-	{ 806400, PLL_2,    3, 0,  192000000, 1100, VDD_RAW(1100) },
+	{ 806400, PLL_2,    3, 0,  192000, 1100, VDD_RAW(1100) },
 #endif
 #endif
 	{ 0 }
